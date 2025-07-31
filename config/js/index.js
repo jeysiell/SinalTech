1567be02219be08e832413fd898e0c97568922c8
jeysiell <gambiarras13feliz@gmail.com>
2025-07-31 08:17:12 -0300

    Update index.js

--- config/js/index.js
+++ config/js/index.js
@@ -97,7 +97,7 @@ function checkSignalTimes(now) {
         sinaisTocadosHoje.add(signalId);
         updateSignalUI(signal, getNextSignal(currentPeriodSignals, signal));
         let dur = signal.duration || 5; 
-        dur = dur === 5 ? 10 : dur === 10 ? 15 : dur;
+        dur = dur === 5 ? 15 : dur === 10 ? 20 : dur;
         initAudio(signal.music || "sino.mp3", dur);
         break;
       }
